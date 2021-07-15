4cfd6f0143b07333149b4f36b12c684ed0850d1f
Andrew Reynolds <andrew_reynolds@mac.com>
2021-07-15 09:29:57 -0700

    custom hook logic added

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,3 +1,32 @@
-const useHttp = () => {};
+import { useState } from "react";
+
+const useHttp = (requestConfig, applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfiq.url, {
+        method: requestConfig.method,
+        headers: requestConfig.headers,
+        body: JSON.stringify(requestConfig.body),
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+  return { sendRequest, isLoading, error };
+};
 
 export default useHttp;
